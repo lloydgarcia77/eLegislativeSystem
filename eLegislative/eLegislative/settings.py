0fb6e9685b6db21318445a07b1afcbb53db04b3a
Lloyd <lloydgarcia77@gmail.com>
2021-07-08 11:05:22 +0800

    stable version 07082021

--- eLegislative/eLegislative/settings.py
+++ eLegislative/eLegislative/settings.py
@@ -28,13 +28,10 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'elegislative_app/media')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '0=8_446=toqa#ix8sv_6rtmk54ajx1paa_fp+xfz_zt_ch2+r%'
+ 
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-ALLOWED_HOSTS = ['elegislativebatangas77.pythonanywhere.com']
-
-# DEBUG = True
-# ALLOWED_HOSTS = ['*']
+DEBUG = True
+ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
@@ -88,27 +85,27 @@ WSGI_APPLICATION = 'eLegislative.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'elegislativebata$elegislative_batangas',
-        # username when creating access from python anywhere
-        'USER': 'elegislativebata',
-        'PASSWORD': 'r00t@passw0rd77',
-        'HOST': 'elegislativebatangas77.mysql.pythonanywhere-services.com',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': 'SET default_storage_engine=INNODB',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'elegislativebata$elegislative_batangas',
+#         # username when creating access from python anywhere
+#         'USER': 'elegislativebata',
+#         'PASSWORD': 'r00t@passw0rd77',
+#         'HOST': 'elegislativebatangas77.mysql.pythonanywhere-services.com',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': 'SET default_storage_engine=INNODB',
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#         },
+#     }
+# }
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
